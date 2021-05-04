e44e5c91e35d57566b1a12afd370031d9c1dbc09
Salar Nosrati-Ershad <snosratiershad@gmail.com>
2021-05-04 11:43:56 +0430

    Add story and commend models

--- tsn2/stories/models.py
+++ tsn2/stories/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.gis.db import models as geo_models
 from mptt.models import MPTTModel, TreeForeignKey
 from uuid import uuid4
 
@@ -10,3 +11,13 @@ class Post(MPTTModel):
 
     created_by = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Story(Post):
+    location = geo_models.PointField()
+    title = models.CharField(max_length=60, null=True, blank=True)
+    description = models.TextField(max_length=255, null=True, blank=True)
+
+
+class Comment(Post):
+    text = models.TextField(max_length=255)
