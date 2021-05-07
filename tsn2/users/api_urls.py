315be68f9f63923891aa8a1b3779facda7a9ff51
Salar Nosrati-Ershad <snosratiershad@gmail.com>
2021-05-08 04:21:22 +0430

    Fix security bugs and Remove user destroy api view

--- tsn2/users/api_urls.py
+++ tsn2/users/api_urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 
-from .api_views import UserRegisterAPIView, UserDestroyAPIView, AuthLoginAPIView, AuthLogoutAPIView
+from .api_views import UserRegisterAPIView, AuthLoginAPIView, AuthLogoutAPIView
 
 user_register = UserRegisterAPIView.as_view()
-user_destroy = UserDestroyAPIView.as_view()
 auth_login = AuthLoginAPIView.as_view()
 auth_logout = AuthLogoutAPIView.as_view()
 
 urlpatterns = [
     path('register/', user_register, name='user_register_api'),
-    path('destroy/<slug:pk>/', user_destroy, name='user_destroy_api'),
     path('login/', auth_login, name='auth_login_api'),
     path('logout/', auth_logout, name='auth_logout_api'),
 ]
--- tsn2/users/api_views.py
+++ tsn2/users/api_views.py
@@ -1,23 +1,16 @@
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 
-from .models import User
 from .serializers import UserSerializer, TokenSerializer
 
 
 class UserRegisterAPIView(CreateAPIView):
     serializer_class = UserSerializer
-
-
-class UserDestroyAPIView(DestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
 
 
 class AuthLoginAPIView(ObtainAuthToken):
@@ -30,6 +23,8 @@ class AuthLoginAPIView(ObtainAuthToken):
 
 
 class AuthLogoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, format=None):
         request.user.auth_token.delete()
-        return Response(response, status=HTTP_204_NO_CONTENT)
+        return Response(status=HTTP_204_NO_CONTENT)
