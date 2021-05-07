c6c5f0f09cd6f103e39c0dab5aa73a36883e9fbd
Salar Nosrati-Ershad <snosratiershad@gmail.com>
2021-05-07 15:23:05 +0430

    Fix url routes

--- tsn2/users/api/urls.py
+++ tsn2/users/api/urls.py
@@ -7,7 +7,7 @@ user_login = UserLoginAPIView.as_view()
 user_logout = UserLogoutAPIView.as_view()
 
 urlpatterns = [
-    path('create/', user_register, name='user_register_api'),
+    path('register/', user_register, name='user_register_api'),
     path('login/', user_login, name='user_login_api'),
     path('logout/', user_logout, name='user_loout_api')
 ]
