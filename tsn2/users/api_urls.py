8a3850ff9096f3967af31af05fdc31412bb2ff44
Salar Nosrati-Ershad <snosratiershad@gmail.com>
2021-05-08 14:09:09 +0430

    Add secured delete user account api

--- tsn2/users/api_urls.py
+++ tsn2/users/api_urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .api_views import UserRegisterAPIView, AuthLoginAPIView, AuthLogoutAPIView
+from .api_views import UserDeleteAPIView, UserRegisterAPIView, AuthLoginAPIView, AuthLogoutAPIView
 
 user_register = UserRegisterAPIView.as_view()
+user_delete = UserDeleteAPIView.as_view()
 auth_login = AuthLoginAPIView.as_view()
 auth_logout = AuthLogoutAPIView.as_view()
 
 urlpatterns = [
     path('register/', user_register, name='user_register_api'),
+    path('delete/', user_delete, name='user_delete_api'),
     path('login/', auth_login, name='auth_login_api'),
     path('logout/', auth_logout, name='auth_logout_api'),
 ]
--- tsn2/users/api_views.py
+++ tsn2/users/api_views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated, DjangoObjectPermissions
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -7,10 +8,22 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 
 from .serializers import UserSerializer, TokenSerializer
+from .models import User
 
 
 class UserRegisterAPIView(CreateAPIView):
     serializer_class = UserSerializer
+
+
+class UserDeleteAPIView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    lookup_url_kwarg = ''
+
+    def get_object(self):
+        return self.request.user
 
 
 class AuthLoginAPIView(ObtainAuthToken):
